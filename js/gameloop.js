fd12c7ba76e4867428fe0ab5eb34fd11d8c9c025
jason-devcode <80931913+jason-devcode@users.noreply.github.com>
2024-11-04 00:41:22 -0500

    feat: add 3d sample

--- js/gameloop.js
+++ js/gameloop.js
@@ -18,7 +18,6 @@ document.addEventListener("keyup", (event) => {
 let posX = 300;
 let posY = 200;
 
-
 const PI = 3.141592;
 const RADIAN = PI / 180.0;
 const HALF_PI = PI / 2;
@@ -33,6 +32,67 @@ export const getCirclePoint = (angle, offsetX, offsetY, factor) => [
   Math.sin(angle) * factor + offsetY,
 ];
 
+export const cubeData = {
+  numVertices: 8,
+  vertices: [
+    [-1, -1, 1],
+    [1, -1, 1],
+    [1, 1, 1],
+    [-1, 1, 1],
+
+    [-1, -1, -1],
+    [1, -1, -1],
+    [1, 1, -1],
+    [-1, 1, -1],
+  ],
+  edges: [
+    // front face
+    [0, 1],
+    [1, 2],
+    [2, 3],
+    [3, 0],
+    // back face
+    [4, 5],
+    [5, 6],
+    [6, 7],
+    [7, 4],
+    // corners
+    [0, 4],
+    [1, 5],
+    [2, 6],
+    [3, 7],
+  ],
+};
+
+export const rotateVertexAxisX = (vertex, angle) => {
+  let vertexOut = [0, 0, 0];
+  vertexOut[0] = vertex[0];
+  vertexOut[2] = vertex[2] * Math.cos(angle) - vertex[1] * Math.sin(angle);
+  vertexOut[1] = vertex[1] * Math.cos(angle) + vertex[2] * Math.sin(angle);
+  return vertexOut;
+};
+export const rotateVertexAxisY = (vertex, angle) => {
+  let vertexOut = [0, 0, 0];
+  vertexOut[0] = vertex[0] * Math.cos(angle) - vertex[2] * Math.sin(angle);
+  vertexOut[1] = vertex[1];
+  vertexOut[2] = vertex[2] * Math.cos(angle) + vertex[0] * Math.sin(angle);
+  return vertexOut;
+};
+export const rotateVertexAxisZ = (vertex, angle) => {
+  let vertexOut = [0, 0, 0];
+  vertexOut[2] = vertex[2];
+  vertexOut[0] = vertex[0] * Math.cos(angle) - vertex[1] * Math.sin(angle);
+  vertexOut[1] = vertex[1] * Math.cos(angle) + vertex[0] * Math.sin(angle);
+  return vertexOut;
+};
+
+export const addVertexToVertex = (vertex, addition) => [
+  vertex[0] + addition[0],
+  vertex[1] + addition[1],
+  vertex[2] + addition[2],
+];
+
+let tempVertices = new Array(cubeData.numVertices);
 
 // Main game loop function
 export const gameLoop = ({ graphics }, deltaTime) => {
@@ -41,28 +101,54 @@ export const gameLoop = ({ graphics }, deltaTime) => {
   // if (keysState["ArrowRight"]) posX += VELOCITY_PX * deltaTime; // Right arrow
   // if (keysState["ArrowDown"]) posY += VELOCITY_PX * deltaTime; // Down arrow
 
-
   if (keysState["ArrowUp"]) currentAngle -= VELOCITY_PX * deltaTime; // Up arrow
   if (keysState["ArrowDown"]) currentAngle += VELOCITY_PX * deltaTime; // Down arrow
 
-  const pointA = getCirclePoint(currentAngle + PI1_4, posX, posY, 100);
-  const pointB = getCirclePoint(currentAngle - HALF_PI, posX, posY, 100);
-  const pointC = getCirclePoint(currentAngle + PI - PI1_4, posX, posY, 100);
+  const viewportWidthHalf = graphics.viewportWidth >> 1;
+  const viewportHeightHalf = graphics.viewportHeight >> 1;
+  const aspectRatio = graphics.viewportHeight / graphics.viewportWidth;
 
+  for (
+    let vertexIterator = 0;
+    vertexIterator < cubeData.numVertices;
+    ++vertexIterator
+  ) {
+    const vertexData = cubeData.vertices[vertexIterator];
 
-  shapes.drawScanLineTriangle(
-    pointA[0],
-    pointA[1],
-    pointB[0],
-    pointB[1],
-    pointC[0],
-    pointC[1],
-    0x0000ff,
-    graphics
-  );
-  shapes.drawCircleFill(pointA[0], pointA[1], 10, 0xff0000);
-  shapes.drawCircleFill(pointB[0], pointB[1], 10, 0xff0000);
-  shapes.drawCircleFill(pointC[0], pointC[1], 10, 0xff0000);
+    let vertex = rotateVertexAxisX(vertexData, currentAngle);
+    vertex = rotateVertexAxisY(vertex, currentAngle * 0.5);
+    vertex = rotateVertexAxisZ(vertex, currentAngle * 0.5);
 
-  // currentAngle += RADIAN * deltaTime;
+    vertex = addVertexToVertex(vertex, [0, 0, 2.5]);
+
+    tempVertices[vertexIterator] = [
+      (vertex[0] / vertex[2]) * viewportWidthHalf * 0.5 * aspectRatio +
+        viewportWidthHalf,
+      (vertex[1] / vertex[2]) * viewportHeightHalf * 0.5 + viewportHeightHalf,
+    ];
+  }
+
+  for (
+    let edgeIterator = 0;
+    edgeIterator < cubeData.edges.length;
+    ++edgeIterator
+  ) {
+    const edge = cubeData.edges[edgeIterator];
+
+    const vertexA = tempVertices[edge[0]];
+    const vertexB = tempVertices[edge[1]];
+
+    shapes.drawNormalizedLine(
+      vertexA[0],
+      vertexA[1],
+      vertexB[0],
+      vertexB[1],
+      0x000000
+    );
+
+    shapes.drawCircleFill(vertexA[0], vertexA[1], 5, 0xff0000);
+    shapes.drawCircleFill(vertexB[0], vertexB[1], 5, 0xff0000);
+  }
+
+  currentAngle += RADIAN * 100 * deltaTime;
 };
