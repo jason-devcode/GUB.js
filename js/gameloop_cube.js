b7848d043053072db9cb8d219147270e23b068dd
jason-devcode <80931913+jason-devcode@users.noreply.github.com>
2024-11-06 23:42:16 -0500

    feat: added cube and donut samples

--- js/gameloop_cube.js
+++ js/gameloop_cube.js
@@ -19,7 +19,6 @@ let posX = 300;
 let posY = 200;
 
 const PI = 3.141592;
-const TAU = PI * 2;
 const RADIAN = PI / 180.0;
 const HALF_PI = PI / 2;
 const PI1_4 = PI / 4;
@@ -32,6 +31,38 @@ export const getCirclePoint = (angle, offsetX, offsetY, factor) => [
   Math.cos(angle) * factor + offsetX,
   Math.sin(angle) * factor + offsetY,
 ];
+
+export const cubeData = {
+  numVertices: 8,
+  vertices: [
+    [-1, -1, 1],
+    [1, -1, 1],
+    [1, 1, 1],
+    [-1, 1, 1],
+
+    [-1, -1, -1],
+    [1, -1, -1],
+    [1, 1, -1],
+    [-1, 1, -1],
+  ],
+  edges: [
+    // front face
+    [0, 1],
+    [1, 2],
+    [2, 3],
+    [3, 0],
+    // back face
+    [4, 5],
+    [5, 6],
+    [6, 7],
+    [7, 4],
+    // corners
+    [0, 4],
+    [1, 5],
+    [2, 6],
+    [3, 7],
+  ],
+};
 
 export const rotateVertexAxisX = (vertex, angle) => {
   let vertexOut = [0, 0, 0];
@@ -83,114 +114,37 @@ export const addVertexToVertex = (vertex, addition) => [
   vertex[2] + addition[2],
 ];
 
-export const generateSolidDonut = (
-  numTransversalCircles = 8,
-  circlesResolution = 16,
-  donutRadius,
-  circlesRadius
-) => {
-  const solidData = {
-    numVertices: 0,
-    numEdges: 0,
-    vertices: [],
-    edges: [],
-  };
-  let circleAngleOffset = 0;
-
-  for (
-    let circlesIterator = 0;
-    circlesIterator < numTransversalCircles;
-    ++circlesIterator
-  ) {
-    const offsetCircle = [
-      Math.cos(circleAngleOffset) * donutRadius,
-      0,
-      Math.sin(circleAngleOffset) * donutRadius,
-    ];
-
-    const currentCircleVerticesStartIndex = solidData.numVertices; // Store the starting index of the current circle
-
-    for (
-      let anglePointCircle = 0;
-      anglePointCircle < TAU;
-      anglePointCircle += TAU / circlesResolution
-    ) {
-      const circleVertex = [
-        Math.cos(anglePointCircle) * circlesRadius,
-        Math.sin(anglePointCircle) * circlesRadius,
-        0,
-      ];
-      let vertex = rotateVertexAxisY(circleVertex, circleAngleOffset);
-      vertex = addVertexToVertex(vertex, offsetCircle);
-
-      solidData.vertices.push(vertex);
-      const currentVertexIndex = solidData.numVertices;
-      solidData.numVertices += 1;
-
-      // Create edges with the current vertex and the previous vertex in the same circle
-      if (circlesIterator > 0) {
-        const previousCircleVertexIndex =
-          currentVertexIndex - circlesResolution;
-        solidData.edges.push([previousCircleVertexIndex, currentVertexIndex]);
-        solidData.numEdges += 1;
-      }
-
-      // Create edges with the current vertex and the next vertex in the same circle
-      const nextVertexIndex =
-        ((currentVertexIndex + 1) % circlesResolution) +
-        circlesIterator * circlesResolution;
-      if (anglePointCircle + TAU / circlesResolution < TAU) {
-        solidData.edges.push([currentVertexIndex, nextVertexIndex]);
-        solidData.numEdges += 1;
-      }
-    }
-
-    // Connect the last vertex of the current circle to the first vertex of the same circle
-    const lastVertexIndex =
-      currentCircleVerticesStartIndex + circlesResolution - 1;
-    solidData.edges.push([lastVertexIndex, currentCircleVerticesStartIndex]);
-    solidData.numEdges += 1;
-
-    circleAngleOffset += TAU / numTransversalCircles;
-  }
-
-  // Connect the last circle to the first circle
-  for (let i = 0; i < circlesResolution; i++) {
-    const firstCircleVertexIndex = i; // Vertex index of the first circle
-    const lastCircleVertexIndex =
-      (numTransversalCircles - 1) * circlesResolution + i; // Vertex index of the last circle
-    solidData.edges.push([lastCircleVertexIndex, firstCircleVertexIndex]);
-    solidData.numEdges += 1;
-  }
-
-  return solidData;
-};
-
-const solidData = generateSolidDonut(64, 64, 0, 1);
-
-let tempVertices = new Array(solidData.numVertices);
+let tempVertices = new Array(cubeData.numVertices);
 
 // Main game loop function
 export const gameLoop = ({ graphics }, deltaTime) => {
+  // if (keysState["ArrowLeft"]) posX -= VELOCITY_PX * deltaTime; // Left arrow
+  // if (keysState["ArrowUp"]) posY -= VELOCITY_PX * deltaTime; // Up arrow
+  // if (keysState["ArrowRight"]) posX += VELOCITY_PX * deltaTime; // Right arrow
+  // if (keysState["ArrowDown"]) posY += VELOCITY_PX * deltaTime; // Down arrow
+
+  if (keysState["ArrowUp"]) currentAngle -= VELOCITY_PX * deltaTime; // Up arrow
+  if (keysState["ArrowDown"]) currentAngle += VELOCITY_PX * deltaTime; // Down arrow
+
   const viewportWidthHalf = graphics.viewportWidth >> 1;
   const viewportHeightHalf = graphics.viewportHeight >> 1;
   const aspectRatio = graphics.viewportHeight / graphics.viewportWidth;
 
   for (
     let vertexIterator = 0;
-    vertexIterator < solidData.numVertices;
+    vertexIterator < cubeData.numVertices;
     ++vertexIterator
   ) {
-    const vertexData = solidData.vertices[vertexIterator];
+    const vertexData = cubeData.vertices[vertexIterator];
 
     let vertex = rotateVertexAxisXYZ(
       vertexData,
       currentAngle,
       currentAngle * 0.5,
-      0
+      currentAngle * 0.5
     );
 
-    vertex = addVertexToVertex(vertex, [0, 0, 2]);
+    vertex = addVertexToVertex(vertex, [0, 0, 2.5]);
 
     tempVertices[vertexIterator] = [
       (vertex[0] / vertex[2]) * viewportWidthHalf * 0.5 * aspectRatio +
@@ -201,15 +155,13 @@ export const gameLoop = ({ graphics }, deltaTime) => {
 
   for (
     let edgeIterator = 0;
-    edgeIterator < solidData.numEdges;
+    edgeIterator < cubeData.edges.length;
     ++edgeIterator
   ) {
-    const edge = solidData.edges[edgeIterator];
+    const edge = cubeData.edges[edgeIterator];
 
     const vertexA = tempVertices[edge[0]];
     const vertexB = tempVertices[edge[1]];
-
-    if (vertexB === undefined) continue;
 
     shapes.drawNormalizedLine(
       vertexA[0],
@@ -219,18 +171,9 @@ export const gameLoop = ({ graphics }, deltaTime) => {
       0x000000
     );
 
-    // shapes.drawCircleFill(vertexA[0], vertexA[1], 2, 0xff0000);
-    // shapes.drawCircleFill(vertexB[0], vertexB[1], 2, 0xff0000);
+    shapes.drawCircleFill(vertexA[0], vertexA[1], 5, 0xff0000);
+    shapes.drawCircleFill(vertexB[0], vertexB[1], 5, 0xff0000);
   }
-  // for (
-  //   let vertexIterator = 0;
-  //   vertexIterator < solidData.numVertices;
-  //   ++vertexIterator
-  // ) {
-  //   const vertex = tempVertices[vertexIterator];
-
-  //   shapes.drawCircleFill(vertex[0], vertex[1], 2, 0xff0000);
-  // }
 
   currentAngle += RADIAN * 100 * deltaTime;
 };
