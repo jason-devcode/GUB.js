24dcd2e303df7b899e4888c4ef68f8c62b2d8d3f
jason-devcode <80931913+jason-devcode@users.noreply.github.com>
2024-11-20 00:42:36 -0500

    feat: update cube example

--- js/app_cube.js
+++ js/app_cube.js
@@ -1,95 +1,44 @@
 import { GameEngine } from "./core/game_engine.js";
 import { GraphicEngine } from "./core/graphic_engine.js";
+
+import { PPMImage } from "./utils/PPMImage.js";
 import { Shapes } from "./utils/Shapes.js";
 
-import { SLATE_950 } from "./utils/colors.js";
+import {
+  GRAY_500,
+  GREEN_500,
+  ORANGE_500,
+  RED_500,
+  SLATE_950,
+  WHITE,
+  YELLOW_500,
+} from "./utils/colors.js";
 
-export const getCirclePoint = (angle, offsetX, offsetY, factor) => [
-  Math.cos(angle) * factor + offsetX,
-  Math.sin(angle) * factor + offsetY,
-];
-
-export const cubeData = {
-  numVertices: 8,
-  vertices: [
-    [-1, -1, 1],
-    [1, -1, 1],
-    [1, 1, 1],
-    [-1, 1, 1],
-
-    [-1, -1, -1],
-    [1, -1, -1],
-    [1, 1, -1],
-    [-1, 1, -1],
-  ],
-  edges: [
-    // front face
-    [0, 1],
-    [1, 2],
-    [2, 3],
-    [3, 0],
-    // back face
-    [4, 5],
-    [5, 6],
-    [6, 7],
-    [7, 4],
-    // corners
-    [0, 4],
-    [1, 5],
-    [2, 6],
-    [3, 7],
-  ],
+/**
+ *
+ * @param {Array<number>} vertex
+ * @param {number} angle
+ */
+export const vertexRotateX = (vertex, angle) => {
+  const vertexOutput = [];
+  vertexOutput[0] = vertex[0];
+  vertexOutput[1] = vertex[1] * Math.cos(angle) - vertex[2] * Math.sin(angle);
+  vertexOutput[2] = vertex[2] * Math.cos(angle) + vertex[1] * Math.sin(angle);
+  return vertexOutput;
 };
 
-export const rotateVertexAxisX = (vertex, angle) => {
-  let vertexOut = [0, 0, 0];
-  vertexOut[0] = vertex[0];
-  vertexOut[2] = vertex[2] * Math.cos(angle) - vertex[1] * Math.sin(angle);
-  vertexOut[1] = vertex[1] * Math.cos(angle) + vertex[2] * Math.sin(angle);
-  return vertexOut;
+/**
+ *
+ * @param {Array<number>} vertex
+ * @param {number} angle
+ */
+export const vertexRotateY = (vertex, angle) => {
+  const vertexOutput = [];
+  vertexOutput[0] = vertex[0] * Math.cos(angle) - vertex[2] * Math.sin(angle);
+  vertexOutput[1] = vertex[1];
+  vertexOutput[2] = vertex[2] * Math.cos(angle) + vertex[0] * Math.sin(angle);
+  return vertexOutput;
 };
-export const rotateVertexAxisY = (vertex, angle) => {
-  let vertexOut = [0, 0, 0];
-  vertexOut[0] = vertex[0] * Math.cos(angle) - vertex[2] * Math.sin(angle);
-  vertexOut[1] = vertex[1];
-  vertexOut[2] = vertex[2] * Math.cos(angle) + vertex[0] * Math.sin(angle);
-  return vertexOut;
-};
-export const rotateVertexAxisZ = (vertex, angle) => {
-  let vertexOut = [0, 0, 0];
-  vertexOut[2] = vertex[2];
-  vertexOut[0] = vertex[0] * Math.cos(angle) - vertex[1] * Math.sin(angle);
-  vertexOut[1] = vertex[1] * Math.cos(angle) + vertex[0] * Math.sin(angle);
-  return vertexOut;
-};
-
-export const rotateVertexAxisXYZ = (vertex, angleX, angleY, angleZ) => {
-  let cosX = Math.cos(angleX);
-  let cosY = Math.cos(angleY);
-  let cosZ = Math.cos(angleZ);
-  let sinX = Math.sin(angleX);
-  let sinY = Math.sin(angleY);
-  let sinZ = Math.sin(angleZ);
-
-  let final_x =
-    (vertex[0] * cosY + (vertex[2] * cosX + vertex[1] * -sinX) * -sinY) * cosZ +
-    (vertex[1] * cosX + vertex[2] * sinX) * -sinZ;
-
-  let final_y =
-    (vertex[1] * cosX + vertex[2] * sinX) * cosZ +
-    (vertex[0] * cosY + (vertex[2] * cosX + vertex[1] * -sinX) * -sinY) * sinZ;
-
-  let final_z =
-    (vertex[2] * cosX + vertex[1] * -sinX) * cosY + vertex[0] * sinY;
-
-  return [final_x, final_y, final_z];
-};
-
-export const addVertexToVertex = (vertex, addition) => [
-  vertex[0] + addition[0],
-  vertex[1] + addition[1],
-  vertex[2] + addition[2],
-];
 
 /**
  * Initializes the game application context, setting up the main game loop.
@@ -99,16 +48,54 @@ export const addVertexToVertex = (vertex, addition) => [
  * @param {Shapes} params.shapes - Instance of the Shapes class for drawing shapes.
  * @returns {function(number): void} Game loop function that takes `deltaTime` to update the game.
  */
-export const gameAppContext = ({ graphics, shapes }) => {
+export const gameAppContext = async ({ graphics, shapes }) => {
   graphics.setClearFramebufferColor(SLATE_950);
+  graphics.enableDepthBufferRendering();
 
   const RADIAN = 0.01745329251;
-  const PI = 3.141592;
+  const aspectRatio = graphics.viewportHeight / graphics.viewportWidth;
 
   let currentAngle = 0.0;
-  const viewportWidthHalf = graphics.viewportWidth >> 1;
-  const viewportHeightHalf = graphics.viewportHeight >> 1;
-  const aspectRatio = graphics.viewportHeight / graphics.viewportWidth;
+
+  const cubeData = {
+    vertices: [
+      [-1, -1, 1],
+      [1, -1, 1],
+      [1, 1, 1],
+      [-1, 1, 1],
+      [-1, -1, -1],
+      [1, -1, -1],
+      [1, 1, -1],
+      [-1, 1, -1],
+    ],
+    triangles: [
+      // front face
+      [0, 1, 2, RED_500],
+      [0, 2, 3, RED_500],
+
+      // back face
+      [4, 5, 6, YELLOW_500],
+      [4, 6, 7, YELLOW_500],
+
+      // top face
+      [4, 5, 1, WHITE],
+      [4, 1, 0, WHITE],
+
+      // bottom face
+      [7, 6, 2, GREEN_500],
+      [7, 2, 3, GREEN_500],
+
+      // left face
+      [4, 0, 3, GRAY_500],
+      [4, 3, 7, GRAY_500],
+
+      // right face
+      [1, 5, 6, ORANGE_500],
+      [1, 6, 2, ORANGE_500],
+    ],
+    numTriangles: 12,
+    numVertices: 8,
+  };
 
   let tempVertices = new Array(cubeData.numVertices);
 
@@ -123,45 +110,66 @@ export const gameAppContext = ({ graphics, shapes }) => {
       vertexIterator < cubeData.numVertices;
       ++vertexIterator
     ) {
-      const vertexData = cubeData.vertices[vertexIterator];
-
-      let vertex = rotateVertexAxisXYZ(
-        vertexData,
-        currentAngle,
-        currentAngle * 0.5,
-        currentAngle * 0.5
+      let vertex = vertexRotateY(
+        cubeData.vertices[vertexIterator],
+        currentAngle
       );
 
-      vertex = addVertexToVertex(vertex, [0, 0, 2.5]);
+      vertex = vertexRotateX(vertex, currentAngle * 0.5);
 
-      tempVertices[vertexIterator] = [
-        (vertex[0] / vertex[2]) * viewportWidthHalf * 0.5 * aspectRatio +
-          viewportWidthHalf,
-        (vertex[1] / vertex[2]) * viewportHeightHalf * 0.5 + viewportHeightHalf,
-      ];
+      vertex[2] += 2.5;
+      // vertex[0] += 2;
+      vertex[0] /= vertex[2] || 1;
+      vertex[1] /= vertex[2] || 1;
+
+      let px =
+        (vertex[0] * graphics.viewportWidth * aspectRatio) / 4 +
+        graphics.viewportWidth / 2;
+      let py =
+        (vertex[1] * graphics.viewportHeight) / 4 + graphics.viewportHeight / 2;
+
+      tempVertices[vertexIterator] = [px, py, vertex[2]];
     }
 
-    for (
-      let edgeIterator = 0;
-      edgeIterator < cubeData.edges.length;
-      ++edgeIterator
-    ) {
-      const edge = cubeData.edges[edgeIterator];
+    // triangle drawing
+    for (let triangle of cubeData.triangles) {
+      const vertexA = tempVertices[triangle[0]];
+      const vertexB = tempVertices[triangle[1]];
+      const vertexC = tempVertices[triangle[2]];
+      const color = triangle[3];
 
-      const vertexA = tempVertices[edge[0]];
-      const vertexB = tempVertices[edge[1]];
-
-      shapes.drawNormalizedLine(
+      shapes.drawScanLineDepthTriangle(
         vertexA[0],
         vertexA[1],
+        vertexA[2],
         vertexB[0],
         vertexB[1],
-        0xffffff
+        vertexB[2],
+        vertexC[0],
+        vertexC[1],
+        vertexC[2],
+        color
       );
 
-      shapes.drawCircleFill(vertexA[0], vertexA[1], 5, 0xff0000);
-      shapes.drawCircleFill(vertexB[0], vertexB[1], 5, 0xff0000);
+      // shapes.drawLine(vertexA[0], vertexA[1], vertexB[0], vertexB[1], 0xeeff00);
+      // shapes.drawLine(vertexB[0], vertexB[1], vertexC[0], vertexC[1], 0xeeff00);
+      // shapes.drawLine(vertexC[0], vertexC[1], vertexA[0], vertexA[1], 0xeeff00);
+
+      // shapes.drawCircleFill(vertexA[0], vertexA[1], 5, 0xffff00);
+      // shapes.drawCircleFill(vertexB[0], vertexB[1], 5, 0xffff00);
+      // shapes.drawCircleFill(vertexC[0], vertexC[1], 5, 0xffff00);
     }
+
+    // vertex drawing
+    // for (
+    //   let vertexIterator = 0;
+    //   vertexIterator < cubeData.numVertices;
+    //   ++vertexIterator
+    // ) {
+    //   let vertex = tempVertices[vertexIterator];
+
+    //   shapes.drawCircleFill(vertex[0], vertex[1], 10, 0xff0000);
+    // }
 
     currentAngle += RADIAN * 100 * deltaTime;
   };
@@ -174,8 +182,18 @@ export const gameAppContext = ({ graphics, shapes }) => {
  */
 export const main = () => {
   const game = new GameEngine();
-  game.setGameAppContext(gameAppContext);
-  game.startGameLoop();
+
+  // Call setGameAppContext, handling the promise if the context is async
+  game
+    .setGameAppContext(gameAppContext)
+    .then(() => {
+      // Once the game loop context is set, start the game loop
+      game.startGameLoop();
+    })
+    .catch((error) => {
+      // Handle any errors that occur while setting the game loop context
+      console.error("Error setting game loop context:", error);
+    });
 };
 
 main();
